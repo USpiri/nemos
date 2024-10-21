88749a47f7e51280987a3aa97bf64059dddd7c5b
USpiri <urielspiridione135@gmail.com>
2024-10-21 19:56:14 -0300

    🔧 Config: Path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
@@ -25,13 +25,11 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_ENV_*"],
   build: {
     target:
-      // @ts-expect-error process is a nodejs global
       process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
-
-    // @ts-expect-error process is a nodejs global
     minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
-
-    // @ts-expect-error process is a nodejs global
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
