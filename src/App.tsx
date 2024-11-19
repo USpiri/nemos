7fa48eac44436b576b1082c2d956d2eafe17c5e3
USpiri <urielspiridione135@gmail.com>
2024-11-18 23:27:05 -0300

    ✨ Feat: App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Sidebar } from "@/components/sidebar/Sidebar";
+
 function App() {
   return (
-    <>
-      <main className="grid h-screen grid-cols-[auto_1fr]">
-        <Sidebar />
+    <main className="grid h-screen grid-cols-[auto_1fr]">
+      <Sidebar />
+      <div>
+        <div className="flex h-11 items-center justify-center border-b border-neutral-800 print:hidden">
+          TitleBar
+        </div>
         <div>App content</div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
 
