5d4f1bb1c99f3bb7c16aea6dba2de0ab38c28d94
USpiri <urielspiridione135@gmail.com>
2024-11-19 18:50:50 -0300

    ✨ Feat: Button hover and active text colors

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -8,7 +8,7 @@ export const Button = ({
   return (
     <button
       className={cn(
-        "rounded p-1 transition-all hover:bg-neutral-700/30 active:scale-95",
+        "rounded p-1.5 text-stone-400 transition-all hover:bg-neutral-700/30 hover:text-stone-200 active:scale-95 active:text-stone-400",
         className,
       )}
       {...props}
