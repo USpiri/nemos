cc5cf2cfe44aa3028fd156b71d8e8d162bf9ffaa
USpiri <urielspiridione135@gmail.com>
2024-11-21 18:24:20 -0300

    ✨ Feat: Configure typography and css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,72 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "rgb(var(--background))",
-        foreground: "rgb(var(--foreground))",
-        "foreground-darker": "rgb(var(--foreground-darker))",
-        border: "rgb(var(--border-color))",
-        detail: "rgb(var(--detail))",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "foreground-darker": "var(--foreground-darker)",
+        border: "var(--border-color)",
+        detail: "var(--detail)",
       },
       height: {
         topbar: "var(--topbar-height)",
       },
+      typography: ({ theme }) => ({
+        theme: {
+          css: {
+            "--tw-prose-body": "var(--prose-body)",
+            "--tw-prose-headings": "var(--prose-headings)",
+            "--tw-prose-lead": "var(--prose-lead)",
+            "--tw-prose-links": "var(--prose-links)",
+            "--tw-prose-bold": "var(--prose-bold)",
+            "--tw-prose-counters": "var(--prose-counters)",
+            "--tw-prose-bullets": "var(--prose-bullets)",
+            "--tw-prose-hr": "var(--prose-hr)",
+            "--tw-prose-quotes": "var(--prose-quotes)",
+            "--tw-prose-quote-borders": "var(--prose-quote-borders)",
+            "--tw-prose-captions": "var(--prose-caption)",
+            "--tw-prose-code": "var(--prose-code)",
+            "--tw-prose-pre-code": "var(--prose-pre-code)",
+            "--tw-prose-pre-bg": "var(--prose-pre-bg)",
+            "--tw-prose-th-borders": "var(--prose-th-borders)",
+            "--tw-prose-td-borders": "var(--prose-td-borders)",
+
+            code: {
+              background: "var(--code-bg)",
+              borderRadius: theme("borderRadius.DEFAULT"),
+              fontWeight: theme("fontWeight.normal"),
+              "&:not(pre code)": {
+                paddingInline: theme("padding.2"),
+                paddingBlock: theme("padding.1"),
+              },
+              "&::before": {
+                display: "none",
+              },
+              "&::after": {
+                display: "none",
+              },
+            },
+
+            pre: {
+              code: {
+                backgroundColor: "transparent",
+              },
+            },
+
+            "h1, h2, h3,h4, h5, h6": {
+              fontWeight: theme("fontWeight.medium"),
+            },
+
+            blockquote: {
+              fontWeight: theme("fontWeight.normal"),
+              fontStyle: "unset",
+              "p::before, p::after": {
+                display: "none",
+              },
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
