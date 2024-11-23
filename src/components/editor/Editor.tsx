229a5340294c2857f4c992fd19b69ca0199db758
USpiri <urielspiridione135@gmail.com>
2024-11-23 18:37:41 -0300

    ✨ Feat: Separate "fake" content and disable spellchecking

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,6 +1,7 @@
 import cn from "@/utils/cn";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { Extensions } from "./extensions";
+import { content } from "@/utils/fake-content";
 
 interface Props {
   className?: string;
@@ -9,16 +10,19 @@ interface Props {
 const baseClasses =
   "focus:outline-none prose prose-theme max-w-[unset] print:prose-neutral";
 
+// TODO:
+// - Enable spellcheck and allow different languages
+
 export const Editor = ({ className }: Props) => {
   const editor = useEditor({
     extensions: [...Extensions],
     editorProps: {
       attributes: {
         class: cn(baseClasses, className),
+        spellcheck: "false",
       },
     },
-    content:
-      '<h1>Hola</h1><p>¿Cómo estas? <strong>Negrita</strong></p><ul><li><p>asdasd</p></li></ul><hr><ol><li><p>1</p></li><li><p>2</p></li></ol><blockquote><p>Quote</p></blockquote><pre><code>const hola = "Hello world!"</code></pre><p>Inline code: <code>const hola = "Hello world!"</code></p>',
+    content,
   });
 
   if (!editor) return null;
