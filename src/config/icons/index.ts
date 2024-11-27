bdd504dbef0a7437a6532dd162612acb7c10128c
USpiri <urielspiridione135@gmail.com>
2024-11-27 10:32:02 -0300

    ✨ Feat: Add icons configuration

---
+++ src/config/icons/index.ts
@@ -0,0 +1,9 @@
+import { FileText, Image, LucideIcon } from "lucide-react";
+
+// TODO:
+// - Add other files
+
+export const icons: Record<string, LucideIcon> = {
+  ".note": FileText,
+  ".png": Image,
+};
