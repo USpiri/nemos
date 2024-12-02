6466dc0af1c53f43316af8240df5c8079c3d9234
USpiri <urielspiridione135@gmail.com>
2024-12-01 22:14:36 -0300

    ✨ Feat: Add tasks lists

--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -2,9 +2,15 @@ import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import StarterKit from "@tiptap/starter-kit";
 import lowlight from "./lowlight";
 import Placeholder from "@tiptap/extension-placeholder";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
 
 export const Extensions = [
   StarterKit.configure({ codeBlock: false }),
+  TaskList,
+  TaskItem.configure({
+    nested: true,
+  }),
   Placeholder.configure({
     placeholder: "Write something...",
   }),
