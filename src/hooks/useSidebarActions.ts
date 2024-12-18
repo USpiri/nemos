4a4673209e4b90b425eaaa6c59bffa1a48030ee8
USpiri <urielspiridione135@gmail.com>
2024-12-18 13:16:53 -0300

    ✨ Feat: Allow custom paths

--- src/hooks/useSidebarActions.ts
+++ src/hooks/useSidebarActions.ts
@@ -2,27 +2,32 @@ import { useSidebarStore } from "@/store/sidebar/sidebar.store";
 import { createDir, createNote as createNoteAction } from "@/utils/fs";
 import { useNavigate } from "react-router";
 
+// TODO:
+// - Use Optimistic hook to update nodes (addNode) and update all
+// nodes recusively. Implement rust call. This will solve errors when
+// renaming or writing to inner notes from a renamed folder.
+
 export const useSidebarActions = () => {
   const addNode = useSidebarStore((store) => store.addNode);
   const navigate = useNavigate();
 
-  const createNote = async () => {
-    const { path, name } = await createNoteAction("notes-app");
+  const createNote = async (path = "notes-app") => {
+    const { path: notePath, name } = await createNoteAction(path);
     const node = {
-      id: path,
-      parent: "notes-app",
+      id: notePath,
+      parent: path,
       droppable: false,
       text: name!,
     };
     addNode(node);
-    navigate(`/file/${path}`);
+    navigate(`/file/${notePath}`);
   };
 
-  const createFolder = async () => {
-    const { path, name } = await createDir("notes-app");
+  const createFolder = async (path = "notes-app") => {
+    const { path: filePath, name } = await createDir(path);
     const node = {
-      id: path,
-      parent: "notes-app",
+      id: filePath,
+      parent: path,
       droppable: true,
       text: name!,
     };
