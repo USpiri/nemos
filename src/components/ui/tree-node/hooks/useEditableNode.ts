5cd528fb754d6e9668b1081c80392999c2468282
USpiri <urielspiridione135@gmail.com>
2024-12-20 23:17:37 -0300

    🐛 Fix: Moving active note

--- src/components/ui/tree-node/hooks/useEditableNode.ts
+++ src/components/ui/tree-node/hooks/useEditableNode.ts
@@ -28,7 +28,7 @@ export const useEditableNode = (
       setEditing(false);
       await rename(node.id.toString(), target);
       updateNodeName(node.id.toString(), target);
-      if (extension && extension === ".note") navigate(`file/${target}`);
+      if (extension === ".note") navigate(`file/${target}`);
     } catch {
       restoreState();
     }
