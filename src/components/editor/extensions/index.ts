4c522de1340d3f950b12b66044b97ef1e7ff4336
USpiri <urielspiridione135@gmail.com>
2025-01-19 13:41:54 -0300

    ✨ Feat: Drag handle

--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -3,8 +3,9 @@ import Placeholder from "@tiptap/extension-placeholder";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
 import StarterKit from "@tiptap/starter-kit";
-import { Math } from "./math";
+import GlobalDragHandle from "tiptap-extension-global-drag-handle";
 import { CodeBlock } from "./codeblock";
+import { Math } from "./math";
 
 export const Extensions = [
   StarterKit.configure({ codeBlock: false }),
@@ -18,4 +19,5 @@ export const Extensions = [
   }),
   CodeBlock,
   Math,
+  GlobalDragHandle,
 ];
