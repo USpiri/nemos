a3bd337f115dd2babfcae2ff7e7390796d53821d
USpiri <urielspiridione135@gmail.com>
2025-01-20 22:00:27 -0300

    🛑 Remove: Unused Layout component

--- src/app/AppLayout.tsx
+++
@@ -1,4 +0,0 @@
-import { ConfigModal } from "@/components/modal/config/ConfigModal";
-import { Topbar } from "@/components/topbar/Topbar";
-import { Sidebar } from "lucide-react";
-import { Outlet } from "react-router";
