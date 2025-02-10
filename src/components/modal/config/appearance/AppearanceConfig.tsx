0f77d22a8e86683e1edc07179960959d29ac75e2
USpiri <urielspiridione135@gmail.com>
2025-02-10 16:49:53 -0300

    🐛 Fix: System color theme update on theme change

--- src/components/modal/config/appearance/AppearanceConfig.tsx
+++ src/components/modal/config/appearance/AppearanceConfig.tsx
@@ -1,53 +1,50 @@
 import { useEffect, useState } from "react";
+import { create } from "zustand";
 
 type ThemeType = "system" | "dark" | "light";
 
 // TODO:
 // - Load color schemes dynamically
 // - Create a cusotm hook to handle configs
+// - Move zustand store
+
+interface ThemeStore {
+  theme: ThemeType;
+  setTheme: (theme: ThemeType) => void;
+}
+
+const useThemeStore = create<ThemeStore>()((set) => ({
+  theme: (localStorage.getItem("theme") as ThemeType) || "system",
+  setTheme: (theme) => {
+    localStorage.setItem("theme", theme);
+    set({ theme });
+  },
+}));
 
 export const AppearanceConfig = () => {
-  const [theme, setTheme] = useState<ThemeType>(
-    () => (localStorage.getItem("theme") as ThemeType) ?? "system",
-  );
+  const { theme, setTheme } = useThemeStore();
   const [colorScheme, setColorScheme] = useState<string | null>(() =>
     JSON.parse(localStorage.getItem("color-scheme") ?? "null"),
   );
 
-  const media = window.matchMedia("(prefers-color-scheme: dark)");
-
-  const applyTheme = (selectedTheme: ThemeType) => {
-    localStorage.setItem("theme", selectedTheme);
-
-    if (selectedTheme === "system") return;
-    if (selectedTheme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  };
-
-  const onMediaColorChange = () => {
-    const matchDark = media.matches;
-    if (matchDark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  };
-
   useEffect(() => {
-    if (theme === "system") {
-      media.addEventListener("change", onMediaColorChange);
-      applyTheme("system");
-    } else {
-      media.removeEventListener("change", onMediaColorChange);
-      applyTheme(theme);
-    }
+    const root = window.document.documentElement;
+    const systemMedia = window.matchMedia("(prefers-color-scheme: dark)");
 
-    return () => {
-      media.removeEventListener("change", onMediaColorChange);
+    const applyTheme = () => {
+      const systemDark = systemMedia.matches;
+      if (theme === "dark" || (theme === "system" && systemDark)) {
+        root.classList.add("dark");
+      } else {
+        root.classList.remove("dark");
+      }
     };
+    applyTheme();
+
+    if (theme === "system") {
+      systemMedia.addEventListener("change", applyTheme);
+    }
+    return () => systemMedia.removeEventListener("change", applyTheme);
   }, [theme]);
 
   useEffect(() => {
