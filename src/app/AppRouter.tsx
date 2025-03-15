d035fa6efb80e9c70c09bf08c795b562e5a03f0d
USpiri <urielspiridione135@gmail.com>
2025-03-15 20:07:37 -0300

    ✨ Feat: Create default note and navigate to it

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,16 +1,19 @@
 import { RedirectToLastRoute } from "@/components/router-watcher/RouterWatcher";
 import { initialize } from "@/utils/initialize";
 import { useEffect, useState } from "react";
-import { Route, Routes } from "react-router";
+import { Route, Routes, useNavigate } from "react-router";
 import { App } from "./App";
 import { NoFile } from "./no-file/NoFile";
 import { Workspace } from "./workspace/Workspace";
 
 export const AppRouter = () => {
   const [initialized, setInitialized] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    initialize().then(() => setInitialized(true));
+    initialize(({ fileDir }) => {
+      navigate(`/file/${fileDir}`);
+    }).then(() => setInitialized(true));
   }, []);
 
   if (!initialized) return null;
--- src/utils/initialize.ts
+++ src/utils/initialize.ts
@@ -1,10 +1,24 @@
-import { ROOT_FOLDER } from "@/config/constants";
-import { exists, mkdir, BaseDirectory } from "@tauri-apps/plugin-fs";
+import { INDEX_FILE_CONTENT, ROOT_FOLDER } from "@/config/constants";
+import { exists, mkdir, BaseDirectory, create } from "@tauri-apps/plugin-fs";
+import { write } from "./fs";
 
-export const initialize = async () => {
+type AfterInit = ({
+  fileDir,
+  check,
+}: {
+  fileDir: string;
+  check: boolean;
+}) => void;
+
+export const initialize = async (afterInit?: AfterInit) => {
+  const fileDir = `${ROOT_FOLDER}/index.note`;
   const check = await exists(`${ROOT_FOLDER}/`, {
     baseDir: BaseDirectory.Document,
   });
-  if (!check)
+  if (!check) {
     await mkdir(`${ROOT_FOLDER}`, { baseDir: BaseDirectory.Document });
+    await create(fileDir, { baseDir: BaseDirectory.Document });
+    await write(fileDir, { content: INDEX_FILE_CONTENT });
+  }
+  afterInit?.({ fileDir, check });
 };
