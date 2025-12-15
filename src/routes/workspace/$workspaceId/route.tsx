ccd6bce8f2c1e1f23c948ec53d171abad3fc0a0d
USpiri <urielspiridione135@gmail.com>
2025-12-15 18:23:25 -0300

    feat: integrate ScrollArea component into workspace route

--- src/routes/workspace/$workspaceId/route.tsx
+++ src/routes/workspace/$workspaceId/route.tsx
@@ -2,6 +2,7 @@ import { Topbar } from "@/components/layout/Topbar";
 import { Sidebar } from "@/components/layout/Sidebar";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { getWorkspaceTree } from "@/lib/workspace";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const Route = createFileRoute("/workspace/$workspaceId")({
   component: RouteComponent,
@@ -16,9 +17,11 @@ function RouteComponent() {
   return (
     <>
       <Sidebar />
-      <div className="w-full">
+      <div className="grid h-screen w-full grid-rows-[auto_1fr] overflow-hidden">
         <Topbar />
-        <Outlet />
+        <ScrollArea className="h-full overflow-hidden">
+          <Outlet />
+        </ScrollArea>
       </div>
     </>
   );
