39c262f2f2dd37c9083ba148ad5ee87a628d9bfa
USpiri <urielspiridione135@gmail.com>
2025-12-16 19:24:00 -0300

    feat: implement Editor component in NoteId route

--- src/routes/workspace/$workspaceId/notes/$noteId.tsx
+++ src/routes/workspace/$workspaceId/notes/$noteId.tsx
@@ -1,14 +1,24 @@
+import { Editor } from "@/components/editor";
+import { readNote } from "@/lib/notes/service/read";
 import { createFileRoute } from "@tanstack/react-router";
+import { useMemo } from "react";
 
 export const Route = createFileRoute("/workspace/$workspaceId/notes/$noteId")({
   component: NoteIdComponent,
+  loader: ({ params: { workspaceId, noteId } }) =>
+    readNote(`${workspaceId}/${noteId}`),
 });
 
 function NoteIdComponent() {
-  const { workspaceId, noteId } = Route.useParams();
+  const note = Route.useLoaderData();
+  const content = useMemo(() => note.content, [note]);
+
   return (
-    <div>
-      Hello "/workspace/{workspaceId}/notes/{noteId}"!
-    </div>
+    <main>
+      <Editor
+        content={content}
+        className="mx-auto w-full max-w-3xl px-10 py-32"
+      />
+    </main>
   );
 }
