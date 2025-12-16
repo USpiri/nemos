44f10f8b7d44ac7f5fabce10782c4649ff37f4a0
USpiri <urielspiridione135@gmail.com>
2025-12-16 20:08:36 -0300

    chore: remove icon configuration and add application constants

--- src/config/icons/index.ts
+++
@@ -1,9 +0,0 @@
-import { FileText, Image, LucideIcon } from "lucide-react";
-
-// TODO:
-// - Add other files
-
-export const icons: Record<string, LucideIcon> = {
-  ".note": FileText,
-  ".png": Image,
-};
