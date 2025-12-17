4f9ab1929c6f4e6a915a457e97481f0f4e53723f
USpiri <urielspiridione135@gmail.com>
2025-12-16 21:01:10 -0300

    feat: integrate Toaster component into RootComponent

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { Providers } from "@/components/providers";
+import { Toaster } from "@/components/ui/sonner";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
@@ -12,6 +13,7 @@ function RootComponent() {
       <Providers>
         <Outlet />
       </Providers>
+      <Toaster />
       <TanStackRouterDevtools position="bottom-right" />
     </>
   );
