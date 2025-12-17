72aae662621a8e38429b4c5c8c54b457f3590cf3
USpiri <urielspiridione135@gmail.com>
2025-12-17 18:12:01 -0300

    feat: add CommandItem interface for command handling in the editor

---
+++ src/types/command.type.ts
@@ -0,0 +1,11 @@
+import { Editor, Range } from "@tiptap/react";
+import { LucideIcon } from "lucide-react";
+
+export interface CommandItem {
+  title: string;
+  description?: string;
+  icon?: LucideIcon;
+  tags?: string[];
+  command?: (props: { editor: Editor; range: Range }) => void;
+  disabled?: boolean;
+}
