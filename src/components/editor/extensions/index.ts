53e357525d6ccdf17de9eee00df6cd37805f5a8f
USpiri <urielspiridione135@gmail.com>
2025-12-17 18:37:28 -0300

    feat: update editor styles and enhance placeholder for commands in extensions

--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -2,6 +2,8 @@ import { TaskItem, TaskList } from "@tiptap/extension-list";
 import { Focus, Placeholder, Selection } from "@tiptap/extensions";
 import { CodeBlock } from "./codeblock";
 import { StarterKit } from "@tiptap/starter-kit";
+import { SlashCommands, filterCommandItems } from "./commands";
+import { suggestionItems } from "@/config/suggestion-menu";
 
 export const Extensions = [
   StarterKit.configure({
@@ -10,8 +12,17 @@ export const Extensions = [
   }),
   Selection,
   Focus,
-  Placeholder,
+  Placeholder.configure({
+    placeholder: 'Write something or type "/" for commands...',
+  }),
   TaskList,
   TaskItem,
   CodeBlock,
+  SlashCommands.configure({
+    commandItems: suggestionItems,
+    suggestion: {
+      items: ({ query }: { query: string }) =>
+        filterCommandItems(query, suggestionItems),
+    },
+  }),
 ];
