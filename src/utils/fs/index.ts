fe2cc4b1f75855b171d8f196c550e36119866ea7
USpiri <urielspiridione135@gmail.com>
2025-12-18 15:41:15 -0300

    refactor: remove unused file system utility functions and related exports

--- src/utils/fs/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./read";
-export * from "./utils";
-export * from "./write";
-export * from "./rename";
-export * from "./create";
-export * from "./copy.ts";
-export * from "./delete.ts";
--- src/utils/fs/write.ts
+++
@@ -1,7 +0,0 @@
-import { BaseDirectory, writeTextFile } from "@tauri-apps/plugin-fs";
-
-export const write = async (path: string | URL, content: object) => {
-  return await writeTextFile(path, JSON.stringify(content), {
-    baseDir: BaseDirectory.Document,
-  });
-};
