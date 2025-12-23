148809431d50d6c621d4acdee77abb3cc6a5754f
USpiri <urielspiridione135@gmail.com>
2025-12-23 17:18:32 -0300

    feat: add NotePending component

---
+++ src/routes/workspace/$workspaceId/notes/-components/NotePending.tsx
@@ -0,0 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const NotePending = () => {
+  return (
+    <main className="mx-auto w-full max-w-3xl px-10 py-32">
+      <div className="space-y-4">
+        <Skeleton className="h-8 w-3/4" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-2/3" />
+      </div>
+    </main>
+  );
+};
