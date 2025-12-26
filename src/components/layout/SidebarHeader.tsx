051d98ddff3a903d8552e046ebfd0cb04dd09bdb
USpiri <urielspiridione135@gmail.com>
2025-12-26 13:48:35 -0300

    feat: enhance sidebar and workspace actions with note and folder creation

--- src/components/layout/SidebarHeader.tsx
+++ src/components/layout/SidebarHeader.tsx
@@ -1,18 +1,55 @@
+import { useCreateNote } from "@/hooks/use-create-note";
 import {
   SidebarHeader as SidebarHeaderBase,
   SidebarMenu,
   SidebarMenuButton,
 } from "../ui/sidebar";
 import { SquarePen, FolderPlus, Settings } from "lucide-react";
+import { useCreateFolder } from "@/hooks/use-create-folder";
+import { getRouteApi, useNavigate, useRouter } from "@tanstack/react-router";
+import { useCallback } from "react";
+import { getNoteIdFromPath } from "@/lib/notes/utils";
+
+const route = getRouteApi("/workspace/$workspaceId");
 
 export const SidebarHeader = () => {
+  const { workspaceId } = route.useParams();
+  const router = useRouter();
+  const navigate = useNavigate();
+
+  const { createNote } = useCreateNote();
+  const { createFolder } = useCreateFolder();
+
+  const handleCreateNote = useCallback(() => {
+    createNote("new-note", (notePath) => {
+      navigate({
+        to: "/workspace/$workspaceId/notes/$noteId",
+        params: { workspaceId, noteId: getNoteIdFromPath(notePath) },
+      });
+    });
+  }, []);
+
+  const handleCreateFolder = useCallback(() => {
+    createFolder("new-folder", () => {
+      router.invalidate({
+        filter: (route) => route.id === "/workspace/$workspaceId",
+      });
+    });
+  }, []);
+
   return (
     <SidebarHeaderBase className="border-border border-b">
       <SidebarMenu className="h-full flex-row items-center justify-center gap-1">
-        <SidebarMenuButton className="aspect-square w-[unset] items-center justify-center p-0">
+        <SidebarMenuButton
+          className="aspect-square w-[unset] items-center justify-center p-0"
+          onClick={handleCreateNote}
+        >
           <SquarePen className="size-4" />
         </SidebarMenuButton>
-        <SidebarMenuButton className="aspect-square w-[unset] items-center justify-center p-0">
+        <SidebarMenuButton
+          className="aspect-square w-[unset] items-center justify-center p-0"
+          onClick={handleCreateFolder}
+        >
           <FolderPlus className="size-4" />
         </SidebarMenuButton>
         <SidebarMenuButton className="aspect-square w-[unset] items-center justify-center p-0">
--- src/routes/workspace/$workspaceId/-components/WorkspaceActions.tsx
+++ src/routes/workspace/$workspaceId/-components/WorkspaceActions.tsx
@@ -1,14 +1,44 @@
 import { Button } from "@/components/ui/button";
+import { useCreateFolder } from "@/hooks/use-create-folder";
+import { useCreateNote } from "@/hooks/use-create-note";
+import { getNoteIdFromPath } from "@/lib/notes/utils";
+import { getRouteApi, useNavigate, useRouter } from "@tanstack/react-router";
 import { Download, FolderPlus, Plus } from "lucide-react";
+import { useCallback } from "react";
+
+const route = getRouteApi("/workspace/$workspaceId");
 
 export const WorkspaceActions = () => {
+  const { createNote } = useCreateNote();
+  const { createFolder } = useCreateFolder();
+  const { workspaceId } = route.useParams();
+  const router = useRouter();
+  const navigate = useNavigate();
+
+  const handleCreateNote = useCallback(() => {
+    createNote("new-note", (notePath) => {
+      navigate({
+        to: "/workspace/$workspaceId/notes/$noteId",
+        params: { workspaceId, noteId: getNoteIdFromPath(notePath) },
+      });
+    });
+  }, []);
+
+  const handleCreateFolder = useCallback(() => {
+    createFolder("new-folder", () => {
+      router.invalidate({
+        filter: (route) => route.id === "/workspace/$workspaceId",
+      });
+    });
+  }, []);
+
   return (
     <div className="flex flex-row flex-wrap gap-2">
-      <Button>
+      <Button onClick={handleCreateNote}>
         <Plus />
         New Note
       </Button>
-      <Button variant="outline">
+      <Button variant="outline" onClick={handleCreateFolder}>
         <FolderPlus />
         New Folder
       </Button>
