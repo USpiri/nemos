f35320cd5a22d81a61ecab053c34c7d4c3a3f58d
USpiri <urielspiridione135@gmail.com>
2025-12-25 23:52:31 -0300

    feat: add dialog store for managing dialog state and interactions

---
+++ src/store/dialog.store.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+
+export type DialogType = "workspace";
+
+interface DialogState {
+  dialog: DialogType | null;
+  data: Record<string, unknown> | null;
+
+  open: (dialog: DialogType, data?: Record<string, any>) => void;
+  close: () => void;
+  isOpen: (dialog: DialogType) => boolean;
+  getData: () => Record<string, unknown> | null;
+}
+
+export const useDialogStore = create<DialogState>((set, get) => ({
+  dialog: null,
+  data: null,
+  open: (dialog, data) => set({ dialog, data }),
+  close: () => set({ dialog: null, data: null }),
+  isOpen: (dialog) => dialog === get().dialog,
+  getData: () => get().data ?? null,
+}));
