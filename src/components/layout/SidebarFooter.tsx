4877bee1b6f0f4f898235b5db44bc1968c0ed285
USpiri <urielspiridione135@gmail.com>
2025-12-26 14:42:21 -0300

    refactor: replace Settings icon with LogOut

--- src/components/layout/SidebarFooter.tsx
+++ src/components/layout/SidebarFooter.tsx
@@ -1,4 +1,4 @@
-import { Settings } from "lucide-react";
+import { LogOut } from "lucide-react";
 import { SidebarFooter as SidebarFooterBase } from "../ui/sidebar";
 import { WorkspaceSelector } from "../WorkspaceSelector";
 import { Link } from "../ui/link";
@@ -9,7 +9,7 @@ export const SidebarFooter = () => {
     <SidebarFooterBase className="border-border flex h-12 flex-row items-center justify-between border-t p-2">
       <WorkspaceSelector />
       <Link to="/workspace" variant="ghost" size="icon">
-        <Settings className="text-muted-foreground" />
+        <LogOut className="text-muted-foreground" />
       </Link>
     </SidebarFooterBase>
   );
