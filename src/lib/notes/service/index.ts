57f37a9311c2f2943126a2100b020194e548e224
USpiri <urielspiridione135@gmail.com>
2026-01-01 23:14:33 -0300

    feat: rename file

--- src/lib/notes/service/index.ts
+++ src/lib/notes/service/index.ts
@@ -1,6 +1,6 @@
 export * from "./create-folder";
 export * from "./create-note";
-export * from "./copy-note";
+export * from "./copy";
 export * from "./path";
 export * from "./read";
 export * from "./write";
