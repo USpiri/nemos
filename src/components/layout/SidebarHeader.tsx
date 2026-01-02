36a522106cd8fd9f7735a7f128619409a4e3ddd1
USpiri <urielspiridione135@gmail.com>
2026-01-01 23:11:58 -0300

    refactor: utilize useWorkspaceActions for note and folder creation

--- src/components/layout/SidebarHeader.tsx
+++ src/components/layout/SidebarHeader.tsx
@@ -1,54 +1,35 @@
-import { useCreateNote } from "@/hooks/use-create-note";
 import {
   SidebarHeader as SidebarHeaderBase,
   SidebarMenu,
   SidebarMenuButton,
 } from "../ui/sidebar";
 import { SquarePen, FolderPlus, Settings } from "lucide-react";
-import { useCreateFolder } from "@/hooks/use-create-folder";
-import { getRouteApi, useNavigate, useRouter } from "@tanstack/react-router";
-import { useCallback } from "react";
-import { getNoteIdFromPath } from "@/lib/notes";
+import { getRouteApi } from "@tanstack/react-router";
+import { useWorkspaceActions } from "@/hooks/use-workspace-actions";
 
 const route = getRouteApi("/workspace/$workspaceId");
 
 export const SidebarHeader = () => {
   const { workspaceId } = route.useParams();
-  const router = useRouter();
-  const navigate = useNavigate();
 
-  const { createNote } = useCreateNote({ workspace: workspaceId });
-  const { createFolder } = useCreateFolder({ workspace: workspaceId });
-
-  const handleCreateNote = useCallback(() => {
-    createNote("new-note", (notePath) => {
-      navigate({
-        to: "/workspace/$workspaceId/notes/$noteId",
-        params: { workspaceId, noteId: getNoteIdFromPath(notePath) },
-      });
-    });
-  }, []);
-
-  const handleCreateFolder = useCallback(() => {
-    createFolder("new-folder", () => {
-      router.invalidate({
-        filter: (route) => route.id === "/workspace/$workspaceId",
-      });
-    });
-  }, []);
+  const { createNoteAndNavigate, createFolderAndRefresh } = useWorkspaceActions(
+    {
+      workspace: workspaceId,
+    },
+  );
 
   return (
     <SidebarHeaderBase className="border-border border-b">
       <SidebarMenu className="h-full flex-row items-center justify-center gap-1">
         <SidebarMenuButton
           className="aspect-square w-[unset] items-center justify-center p-0"
-          onClick={handleCreateNote}
+          onClick={() => createNoteAndNavigate()}
         >
           <SquarePen className="size-4" />
         </SidebarMenuButton>
         <SidebarMenuButton
           className="aspect-square w-[unset] items-center justify-center p-0"
-          onClick={handleCreateFolder}
+          onClick={() => createFolderAndRefresh()}
         >
           <FolderPlus className="size-4" />
         </SidebarMenuButton>
--- src/routes/workspace/$workspaceId/-components/WorkspaceActions.tsx
+++ src/routes/workspace/$workspaceId/-components/WorkspaceActions.tsx
@@ -1,45 +1,26 @@
 import { Button } from "@/components/ui/button";
-import { useCreateFolder } from "@/hooks/use-create-folder";
-import { useCreateNote } from "@/hooks/use-create-note";
-import { getNoteIdFromPath } from "@/lib/notes";
-import { getRouteApi, useNavigate, useRouter } from "@tanstack/react-router";
+import { useWorkspaceActions } from "@/hooks/use-workspace-actions";
+import { getRouteApi } from "@tanstack/react-router";
 import { Download, FolderPlus, Plus } from "lucide-react";
-import { useCallback } from "react";
 
 const route = getRouteApi("/workspace/$workspaceId");
 
 export const WorkspaceActions = () => {
   const { workspaceId } = route.useParams();
-  const router = useRouter();
-  const navigate = useNavigate();
 
-  const { createNote } = useCreateNote({ workspace: workspaceId });
-  const { createFolder } = useCreateFolder({ workspace: workspaceId });
-
-  const handleCreateNote = useCallback(() => {
-    createNote("new-note", (notePath) => {
-      navigate({
-        to: "/workspace/$workspaceId/notes/$noteId",
-        params: { workspaceId, noteId: getNoteIdFromPath(notePath) },
-      });
-    });
-  }, []);
-
-  const handleCreateFolder = useCallback(() => {
-    createFolder("new-folder", () => {
-      router.invalidate({
-        filter: (route) => route.id === "/workspace/$workspaceId",
-      });
-    });
-  }, []);
+  const { createNoteAndNavigate, createFolderAndRefresh } = useWorkspaceActions(
+    {
+      workspace: workspaceId,
+    },
+  );
 
   return (
     <div className="flex flex-row flex-wrap gap-2">
-      <Button onClick={handleCreateNote}>
+      <Button onClick={() => createNoteAndNavigate()}>
         <Plus />
         New Note
       </Button>
-      <Button variant="outline" onClick={handleCreateFolder}>
+      <Button variant="outline" onClick={() => createFolderAndRefresh()}>
         <FolderPlus />
         New Folder
       </Button>
