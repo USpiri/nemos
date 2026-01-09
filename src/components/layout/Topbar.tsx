8fefd01aabe12732601c99c47339634be26e4aad
USpiri <urielspiridione135@gmail.com>
2026-01-08 22:30:05 -0300

    feat: integrate EditableFilename component into Topbar and WorkspaceTree

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,9 +1,30 @@
+import { useParams } from "@tanstack/react-router";
 import { SidebarTrigger } from "../ui/sidebar";
+import { getNoteNameWithoutExtension } from "@/lib/notes";
+import { FILE_EXTENSION } from "@/config/constants";
+import { useMemo } from "react";
+import { EditableFilename } from "../EditableFilename";
 
 export const Topbar = () => {
+  const { noteId } = useParams({
+    strict: false,
+  });
+
+  const noteName = useMemo(
+    () => (noteId ? getNoteNameWithoutExtension(noteId) : ""),
+    [noteId],
+  );
+
+  if (!noteId) return null;
   return (
-    <div className="border-border flex items-center justify-between border-b px-2.5 py-2">
+    <div className="border-border flex items-center justify-between gap-2 border-b px-2.5 py-2">
       <SidebarTrigger className="size-8" />
+      <EditableFilename
+        display={noteName!}
+        path={noteId}
+        suffix={FILE_EXTENSION}
+        className="p-0"
+      />
     </div>
   );
 };
--- src/components/workspace-tree/WorkspaceTree.tsx
+++ src/components/workspace-tree/WorkspaceTree.tsx
@@ -4,6 +4,8 @@ import { TreeNode } from "./TreeNode";
 import { TreeContextMenu } from "./TreeContextMenu";
 import { ChevronDown, ChevronRight, FileText } from "lucide-react";
 import { getNoteIdFromPath } from "@/lib/notes";
+import { FILE_EXTENSION } from "@/config/constants";
+import { EditableFilename } from "../EditableFilename";
 
 interface Props {
   tree: NodeModel[];
@@ -31,7 +33,12 @@ export const WorkspaceTree = ({ tree, root, workspace }: Props) => {
             ) : (
               <FileText />
             )}
-            {node.text}
+            <EditableFilename
+              display={node.text.replace(FILE_EXTENSION, "")}
+              path={node.id.toString()}
+              suffix={node.droppable ? undefined : FILE_EXTENSION}
+              className="p-0"
+            />
           </TreeNode>
         )}
         onDrop={console.log}
