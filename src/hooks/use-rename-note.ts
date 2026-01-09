60099ac41585c1b3a5a3b75711437631ff1c8676
USpiri <urielspiridione135@gmail.com>
2026-01-08 22:28:26 -0300

    refactor: enhance useRenameFolder and useRenameNote hooks with workspace dependency

--- src/hooks/use-rename-note.ts
+++ src/hooks/use-rename-note.ts
@@ -7,30 +7,33 @@ interface Props {
 }
 
 export const useRenameNote = ({ workspace }: Props) => {
-  const renameNote = useCallback(async (note: string, newName: string) => {
-    if (!note || !newName) {
-      toast.error("Note and the new name are required");
-      return;
-    }
+  const renameNote = useCallback(
+    async (note: string, newName: string) => {
+      if (!note || !newName) {
+        toast.error("Note and the new name are required");
+        return;
+      }
 
-    try {
-      const notePath = await renameNoteFn({ workspace, note, newName });
-      return notePath;
-    } catch (error) {
-      if (error instanceof NoteError) {
-        switch (error.code) {
-          case "RENAME_FAILED":
-            toast.error("Failed to rename note", {
-              description: error.message,
-            });
-            break;
-          case "NOT_FOUND":
-            toast.error("Note not found");
-            break;
+      try {
+        const notePath = await renameNoteFn({ workspace, note, newName });
+        return notePath;
+      } catch (error) {
+        if (error instanceof NoteError) {
+          switch (error.code) {
+            case "RENAME_FAILED":
+              toast.error("Failed to rename note", {
+                description: error.message,
+              });
+              break;
+            case "NOT_FOUND":
+              toast.error("Note not found");
+              break;
+          }
         }
       }
-    }
-  }, []);
+    },
+    [workspace],
+  );
 
   return { renameNote };
 };
