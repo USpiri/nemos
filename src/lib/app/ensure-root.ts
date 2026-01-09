b4114e484c33be0e6d4bc14be2accf9bd96cd9f2
USpiri <urielspiridione135@gmail.com>
2026-01-09 13:05:15 -0300

    chore: remove old ensureRoot function

--- src/lib/app/ensure-root.ts
+++
@@ -1,7 +0,0 @@
-import { createDir, exists } from "../fs";
-import { ROOT } from "@/config/constants";
-
-export const ensureRoot = async () => {
-  const check = await exists(ROOT);
-  if (!check) return await createDir(ROOT);
-};
