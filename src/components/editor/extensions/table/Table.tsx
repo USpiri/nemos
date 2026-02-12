de69973294f72336d07db6aa66c369ffe751c65d
USpiri <urielspiridione135@gmail.com>
2026-02-12 17:45:08 -0300

    feat: implement TableNodeView with add row and column functionality, and remove deprecated version

---
+++ src/components/editor/extensions/table/Table.tsx
@@ -0,0 +1,49 @@
+import { NodeViewContent, NodeViewProps, NodeViewWrapper } from '@tiptap/react'
+import { Plus } from 'lucide-react'
+import { useCallback } from 'react'
+import { Button } from '@/components/ui/button'
+import { isInsideNode } from '@/lib/editor/utils'
+
+export default function TableNodeView({ node, editor, getPos }: NodeViewProps) {
+  const addColumn = useCallback(
+    () => editor.chain().focus().addColumnAfter().run(),
+    [editor],
+  )
+  const addRow = useCallback(
+    () => editor.chain().focus().addRowAfter().run(),
+    [editor],
+  )
+
+  return (
+    <NodeViewWrapper className="tableWrapper">
+      <NodeViewContent />
+      {isInsideNode(
+        editor.state.selection.from,
+        editor.state.selection.to,
+        getPos() ?? 0,
+        node.nodeSize,
+      ) && (
+        <>
+          <Button
+            onClick={() => addRow()}
+            variant="ghost"
+            size="icon-xs"
+            className="absolute bottom-0 z-10 w-full translate-y-full"
+            tabIndex={-1}
+          >
+            <Plus className="size-4" />
+          </Button>
+          <Button
+            onClick={() => addColumn()}
+            variant="ghost"
+            size="icon-xs"
+            className="absolute top-0 left-full z-10 h-full"
+            tabIndex={-1}
+          >
+            <Plus className="size-4" />
+          </Button>
+        </>
+      )}
+    </NodeViewWrapper>
+  )
+}
--- src/components/editor/extensions/table/index.ts
+++ src/components/editor/extensions/table/index.ts
@@ -1,29 +1,70 @@
-import { TableKit } from '@tiptap/extension-table'
+import {
+  TableCell,
+  Table as TableExtension,
+  TableHeader,
+  TableKitOptions,
+  TableRow,
+} from '@tiptap/extension-table'
+import { Extension, ReactNodeViewRenderer } from '@tiptap/react'
+import TableNodeView from './Table'
 
 // TODO:
 // - Delete column/row by pressing backspace when empty row/column is selected
 // - Escape from table by pressing tab on empty row
 // - Add table node view
 
-export const Table = TableKit.configure({
-  table: {
-    allowTableNodeSelection: true,
-    renderWrapper: true,
-  },
-}).extend({
-  addKeyboardShortcuts() {
-    return {
-      ...this.parent?.(),
-      'Shift-Control-Enter': () => this.editor.commands.addRowBefore(),
-      'Control-Enter': () => this.editor.commands.addRowAfter(),
-      'Shift-Control-Tab': () =>
-        this.editor.chain().addColumnBefore().goToPreviousCell().run(),
-      'Control-Tab': () => {
-        return this.editor.chain().addColumnAfter().goToNextCell().run()
-      },
+// export const Table = TableKit.configure({
+//   table: {
+//     allowTableNodeSelection: true,
+//     renderWrapper: true,
+//   },
+// }).extend({
+
+//   addKeyboardShortcuts() {
+//     return {
+//       ...this.parent?.(),
+//       'Shift-Control-Enter': () => this.editor.commands.addRowBefore(),
+//       'Control-Enter': () => this.editor.commands.addRowAfter(),
+//       'Shift-Control-Tab': () =>
+//         this.editor.chain().addColumnBefore().goToPreviousCell().run(),
+//       'Control-Tab': () => {
+//         return this.editor.chain().addColumnAfter().goToNextCell().run()
+//       },
+//     }
+//   },
+//   onUpdate() {
+//     this.editor.commands.fixTables()
+//   },
+// })
+
+export const Table = Extension.create<TableKitOptions>({
+  name: 'tableKit',
+  addExtensions() {
+    const extensions = []
+    if (this.options.table !== false) {
+      extensions.push(
+        TableExtension.configure({
+          allowTableNodeSelection: true,
+          // renderWrapper: true,
+        }).extend({
+          addNodeView() {
+            return ReactNodeViewRenderer(TableNodeView, {
+              // as: 'table',
+              contentDOMElementTag: 'table',
+            })
+          },
+        }),
+      )
     }
-  },
-  onUpdate() {
-    this.editor.commands.fixTables()
+    if (this.options.tableCell !== false) {
+      extensions.push(TableCell.configure(this.options.tableCell))
+    }
+    if (this.options.tableHeader !== false) {
+      extensions.push(TableHeader.configure(this.options.tableHeader))
+    }
+    if (this.options.tableRow !== false) {
+      extensions.push(TableRow.configure(this.options.tableRow))
+    }
+    return extensions
   },
 })
