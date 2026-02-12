7f41d8304af38dfb35b999329fa1509f36712ac5
USpiri <urielspiridione135@gmail.com>
2026-02-12 15:38:42 -0300

    feat: implement resizable image component with alignment options

---
+++ src/components/editor/extensions/image/Image.tsx
@@ -0,0 +1,73 @@
+import { NodeViewProps, NodeViewWrapper } from '@tiptap/react'
+import { AlignCenter, AlignLeft, AlignRight, GripVertical } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+const MIN_WIDTH = 200
+
+const alignButtons: {
+  value: 'start' | 'end' | 'center'
+  icon: React.ElementType
+}[] = [
+  { value: 'start', icon: AlignLeft },
+  { value: 'center', icon: AlignCenter },
+  { value: 'end', icon: AlignRight },
+]
+
+export default function ImageNodeView({
+  node,
+  editor,
+  selected,
+}: NodeViewProps) {
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    const parent = event.currentTarget.parentElement
+    if (!parent) return
+    const image = parent.querySelector('img')
+    if (!image) return
+
+    const startSize = { x: image.clientWidth, y: image.clientHeight }
+    const startPosition = { x: event.pageX, y: event.pageY }
+
+    function onMouseMove(mouseMoveEvent: MouseEvent) {
+      const newWidth = startSize.x - startPosition.x + mouseMoveEvent.pageX
+      if (newWidth < MIN_WIDTH) return
+      editor.commands.updateAttributes('image', {
+        width: newWidth,
+        height: startSize.y - startPosition.y + mouseMoveEvent.pageY,
+      })
+    }
+    function onMouseUp() {
+      document.body.removeEventListener('mousemove', onMouseMove)
+    }
+
+    document.body.addEventListener('mousemove', onMouseMove)
+    document.body.addEventListener('mouseup', onMouseUp, { once: true })
+  }
+
+  return (
+    <NodeViewWrapper
+      as="figure"
+      style={{ textAlign: node.attrs.align }}
+      className={cn('image-resizable', selected && 'has-focus')}
+    >
+      <img {...node.attrs} />
+      <div className="image-resize-trigger" onMouseDown={handleMouseDown}>
+        <GripVertical />
+      </div>
+      <div className="image-align-buttons">
+        {alignButtons.map((e) => (
+          <Button
+            variant={e.value === node.attrs.align ? 'default' : 'outline'}
+            data-align={e.value}
+            data-selected={e.value === node.attrs.align}
+            size="icon"
+            key={e.value}
+            onClick={() => editor.chain().setImageAlign(e.value).run()}
+          >
+            <e.icon />
+          </Button>
+        ))}
+      </div>
+    </NodeViewWrapper>
+  )
+}
--- src/components/editor/extensions/image/index.ts
+++ src/components/editor/extensions/image/index.ts
@@ -1,5 +1,41 @@
 import { Image as ImageExtension } from '@tiptap/extension-image'
+import { ReactNodeViewRenderer } from '@tiptap/react'
+import ImageNodeView from './Image'
+
+declare module '@tiptap/react' {
+  interface Commands<ReturnType> {
+    imageBlock: {
+      setImageWidth: (width: string) => ReturnType
+      setImageAlign: (align: 'start' | 'center' | 'end') => ReturnType
+    }
+  }
+}
 
 export const Image = ImageExtension.configure({
   allowBase64: true,
+}).extend({
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      align: { default: 'center', renderHTML: ({ align }) => ({ align }) },
+    }
+  },
+
+  addCommands() {
+    return {
+      setImageWidth:
+        (width) =>
+        ({ commands }) =>
+          commands.updateAttributes('image', { width }),
+
+      setImageAlign:
+        (align) =>
+        ({ commands }) =>
+          commands.updateAttributes('image', { align }),
+    }
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(ImageNodeView)
+  },
 })
