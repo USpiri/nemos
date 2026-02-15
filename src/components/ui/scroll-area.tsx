93326a16dd2415894ed9f12539f32d75bd7d170a
USpiri <urielspiridione135@gmail.com>
2026-02-15 20:24:23 -0300

    refactor: sort classes

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -21,7 +21,7 @@ function ScrollArea({
     >
       <ScrollAreaViewport
         data-slot="scroll-area-viewport"
-        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
+        className="size-full rounded-[inherit] outline-none transition-[color,box-shadow] focus-visible:outline-1 focus-visible:ring-[3px] focus-visible:ring-ring/50"
       >
         {children}
       </ScrollAreaViewport>
@@ -42,14 +42,14 @@ function ScrollBar({
       data-orientation={orientation}
       orientation={orientation}
       className={cn(
-        'w-2 data-horizontal:h-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:h-full data-vertical:w-2.5 data-vertical:border-l data-vertical:border-l-transparent flex touch-none p-px transition-colors select-none',
+        'flex w-2 touch-none select-none p-px transition-colors data-horizontal:h-2.5 data-vertical:h-full data-vertical:w-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:border-l data-vertical:border-l-transparent',
         className,
       )}
       {...props}
     >
       <ScrollAreaThumb
         data-slot="scroll-area-thumb"
-        className="rounded-full bg-border relative flex-1"
+        className="relative flex-1 rounded-full bg-border"
       />
     </ScrollAreaScrollbar>
   )
