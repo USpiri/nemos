8138279c28da67c8c44b5e6b14169f8ff4944742
USpiri <urielspiridione135@gmail.com>
2026-05-23 18:33:50 -0300

    fix: change default file extension from 'note' to 'md'

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const ROOT = 'nemos-app'
-export const EXTENSION = 'note'
+export const EXTENSION = 'md'
 export const RECENT_NOTES_LIMIT = 10
 export const DEFAULT_NOTE_NAME = 'new-note'
 export const DEFAULT_FOLDER_NAME = 'new-folder'
