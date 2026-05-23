175933689e2fa20b2226f56d69d70177d0a2d902
USpiri <urielspiridione135@gmail.com>
2026-05-23 18:53:49 -0300

    feat: add FrontmatterSchema to note schema and update related types

--- src/lib/notes/note.schema.ts
+++ src/lib/notes/note.schema.ts
@@ -1,6 +1,14 @@
 import { z } from 'zod'
 
+export const FrontmatterSchema = z
+  .object({
+    readonly: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
+    cssClass: z.string().optional(),
+  })
+  .loose()
+
 export const NoteSchema = z.object({
+  frontmatter: FrontmatterSchema,
   content: z.string(),
-  readonly: z.boolean().optional(),
 })
