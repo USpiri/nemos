ef788773ec48f982394484a650e90c9fd297f7b1
USpiri <urielspiridione135@gmail.com>
2026-06-04 16:57:32 -0300

    feat: render SidebarResizeHandle in Sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,10 +2,12 @@ import { Sidebar as SidebarBase } from '../ui/sidebar'
 import { SidebarContent } from './SidebarContent'
 import { SidebarFooter } from './SidebarFooter'
 import { SidebarHeader } from './SidebarHeader'
+import { SidebarResizeHandle } from './SidebarResizeHandle'
 
 export const Sidebar = () => {
   return (
     <SidebarBase>
+      <SidebarResizeHandle />
       <SidebarHeader />
       <SidebarContent />
       <SidebarFooter />
