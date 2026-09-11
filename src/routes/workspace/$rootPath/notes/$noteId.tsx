50225fc3789bdb4f05cf60c8e2cdff25322bb416
USpiri <99831646+USpiri@users.noreply.github.com>
2026-09-11 15:27:54 -0300

    Revert "fix: add print stylesheet to hide app chrome and fit content to the page"

--- src/routes/workspace/$rootPath/notes/$noteId.tsx
+++ src/routes/workspace/$rootPath/notes/$noteId.tsx
@@ -68,7 +68,7 @@ function NoteView({
     <main className={cn('note h-full', frontmatter.cssClass)}>
       <Suspense fallback={<NotePending />}>
         <NoteProperties
-          className="mx-auto w-full max-w-3xl px-10 pt-20 print:hidden"
+          className="mx-auto w-full max-w-3xl px-10 pt-20"
           frontmatter={frontmatter}
           onChange={handleFrontmatterChange}
         />
